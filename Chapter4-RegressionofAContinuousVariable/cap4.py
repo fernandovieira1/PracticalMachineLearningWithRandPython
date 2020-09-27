d2b2d475562403cf72601fe1ec9e5bde3015356a
Fernando Vieira <fernandovieira@iMac-de-Fernando.local>
2020-09-27 01:21:07 -0400

    até regressão linear múltipla (estudar KNN agora, antes de continuar)

--- Chapter4-RegressionofAContinuousVariable/cap4.py
+++ Chapter4-RegressionofAContinuousVariable/cap4.py
@@ -15,15 +15,12 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-
-
 ## Ler os dados
 df = pd.read_csv("Boston.csv", encoding="ISO-8859-1") # Considerando o pacote "RFunctions.R", o nome do data frame sempre deve ser 'df'
 
 df.columns # https://www.cs.toronto.edu/~delve/data/boston/bostonDetail.html
 
 ### REGRESSÃO SIMPLES
-
 # Selecionar y e x
 y = df["medv"]
 X = df["lstat"]
@@ -63,3 +60,40 @@ fig.figure.savefig('foo.png', bbox_inches='tight')
 fig1.figure.savefig('foo1.png', bbox_inches='tight')
 plt.show()
 
+### REGRESSÃO MÚLTIPLA
+# Ler os dados
+crimesDF = pd.read_csv("crimes.csv", encoding="ISO-8859-1")
+crimesDF
+
+crimesDF.columns # https://rpubs.com/ksakwa/470184
+
+# Remover as 7 primeiras colunas
+crimesDF1 = crimesDF.iloc[:,7:crimesDF.shape[1]]
+
+# Convert to numeric  
+crimesDF2 = crimesDF1.apply(pd.to_numeric, errors='coerce')  
+
+# Impute NA to 0s  
+crimesDF2.fillna(0, inplace=True)
+
+# Selecionar y e x
+y = crimesDF2.iloc[:,121]
+X = crimesDF2.iloc[:,0:120]
+
+## Dividir os dados entre dados de treino e de teste (75, 25)
+X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, random_state=0)
+X_treino
+
+## Regressão linear 
+# y: Median value of owner-occupied homes in $1000's
+# x: lower status of the population
+linreg = LinearRegression().fit(X_treino, y_treino)
+linreg.score(X_treino, y_treino)
+linreg.intercept_
+linreg.coef_
+
+r2Treino = linreg.score(X_treino, y_treino)
+r2Treino
+
+r2Teste = linreg.score(X_teste, y_teste)
+r2Teste
--- Chapter4-RegressionofAContinuousVariable/cap4.r
+++ Chapter4-RegressionofAContinuousVariable/cap4.r
@@ -40,3 +40,45 @@ rsquared <- Rsquared(linreg,teste,teste$medv)
 rsquared
 
 round(Rsquared(linreg,treino,treino$medv), 4)
+
+### REGRESSÃO MÚLTIPLA
+## Ler os dados
+crimesDF <- read.csv("crimes.csv")
+head(crimesDF)
+tail(crimesDF)
+
+names(crimesDF) # https://rpubs.com/ksakwa/470184
+
+# Remover as 7 primeiras colunas
+crimesDF1 <- crimesDF[,7:length(crimesDF)]
+
+# Converter todos para numérico
+crimesDF2 <- sapply(crimesDF1, as.numeric)
+crimesDF2
+
+# Procurar por NAs
+a <- is.na(crimesDF2)
+
+# Substituir NAs por 0
+crimesDF2[a] <- 0
+
+# Criar como dataframe
+crimesDF2 <- as.data.frame(crimesDF2)
+
+# Separar treino e teste
+treino_idx <- trainTestSplit(crimesDF2, trainPercent=75, seed=5)
+treino <- crimesDF2[treino_idx,]
+teste <- crimesDF2[-treino_idx,]
+
+dim(treino)
+dim(teste)
+
+## Regressão linear 
+# y: ViolentCrimesPerPop
+# x: Todos os outros
+
+linreg <- lm(ViolentCrimesPerPop~., data=treino)
+summary(linreg, digits=3)
+
+rsquared <- Rsquared(linreg, teste, teste$ViolentCrimesPerPop)
+rsquared
